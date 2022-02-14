da549338979cd4fba3c68aa1f0ce4203c1c2acda
Diego Gonzalez <degonzalez6@gmail.com>
2022-02-14 04:27:22 -0300

    Commit con los cambios del video 6

--- main.js
+++ main.js
@@ -6,13 +6,16 @@
         this.game_over= false;
         this.bars=[];
         this.ball = null;
+        this.playing = false;
+
+
     }
 
 self.Board.prototype =
 {
     get elements(){
-        var elements = this.bars;
-      //  elements.push(this.ball);
+        var elements = this.bars.map(function(bar){return bar;});
+        elements.push(this.ball);
         return elements;
     }
 }
@@ -24,15 +27,45 @@ self.Board.prototype =
         this.x = x;
         this.y = y;
         this.radius = radius;
-
         this.speed_y=0;
         this.speed_x=3;
         this.board = board;
+        this.direction = 1;
+        this.bounce_angle =0;
+        this.max_bounce_angle = Math.PI /12;
+        this.speed = 3;
+
 
         board.ball = this;
 
         this.kind = "circle";
 
+        self.Ball.prototype={
+            move: function(){
+                this.x += (this.speed_x * this.direction);
+                this.y += (this.speed_y);
+            },
+            get width(){
+                return this.radius * 2;
+            },
+            get height(){
+                return this.radius * 2;
+            }
+            collision: function(bar){
+                //Reacciona a una colisión
+                var relative_intersect_y = (bar.y + (bar.height/2))-this.y;
+                var normalized_intersect_y= relative_intersect_y /(bar.height/2);
+
+                this.bounce_angel = normalized_intersect_y*this.max_bounce_angle;
+
+                this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+                this.speed_x = this.speed * Math.cos(this.max_bounce_angle);
+
+                if(this.x>(this.board.width/2))this.direction=-1;
+                else this.direction=1;
+
+            }
+        }
     }
 })();
 
@@ -70,7 +103,7 @@ self.Board.prototype =
     } 
     self.BoardView.prototype = {
         clean: function(){
-            this.ctx.clearRect(0,0,this.board.width, this.board.height);
+            this.ctx.clearRect(0,0,this.board.width,this.board.height);
         },
         draw:function(){
             for(var i = this.board.elements.length-1; i >=0; i--){
@@ -79,12 +112,49 @@ self.Board.prototype =
                 draw(this.ctx, el);
             };
         },
-        play: function(){
-            this.clean();
-            this.draw();
-        }
+        check_collisions: function(){
+            for(var i = this.board.bars.length - 1; i >= 0; i--){
+                var bar = this.board.bars[i];
+                if(hit(bar, this.board.ball)){
+                    this.board.ball.collision(bar);
 
+                }
+            };
+        },
+        play: function(){
+            if(this.board.playing){
+                this.clean();
+                this.draw();
+                this.check_collisions();
+                this.board.ball.move();
+            }
+  
+        }
     }
+
+    function hit(a,b){
+        //Revisa si A colisiona con B
+        var hit = false;
+
+        //Colisiones horizontales
+        if(b.x + b.width >= a.x && b.x < a.x + a.width){
+            //Colisiones verticales
+            if(b.y + b.height >= a.y && b.y < a.y + a.height)
+                hit = true; 
+        }
+        //Colisiones de a con b
+        if(b.x <= a.x && b.x +b.width >= a.x + a.width){
+            if(b.y <= a.y && b.y + b.height >= a.y + a.height)
+            hit=true;
+        }
+        //Colision de b con a
+        if(a.x <= b.x && a.x + a.width >= b.x + b.width){
+            if(a.y <= b.y && a.y + a.height >= b.y + b.height)
+                hit.true;
+        }
+        return hit;
+    }
+
     function draw(ctx, element){
         //if(element !== null && element.hasOwnProperty("kind")){
             switch(element.kind){
@@ -118,16 +188,23 @@ document.addEventListener("keydown", function(ev){
     }
     else if(ev.keyCode==40){
         bar.down;
-    }else if(ev.keyCode==87){
+    }else if(ev.keyCode===87){
         bar2.up;
-    }else if(ev.keyCode==83){
+    }else if(ev.keyCode===83){
         bar2.down;
-    }
-console.log(bar.toString);
+    }else if(ev.keyCode===32){
+        ev.preventDefault();
+        board.playing = !board.playing;
+    } 
 });
+
+board_view.draw();
 
 window.requestAnimationFrame(controller);
 
+setTimeout(function(){
+    ball.direction=-1;
+}, 4000);
 function controller(){
     
     board_view.play();
